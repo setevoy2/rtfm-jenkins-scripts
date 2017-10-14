851c55f20c2755f0f5d93e0b626a364c232b1ca8
Arseny Zinchenko <reg@setevoy.kiev.ua>
2017-10-14 18:45:33 +0300

    provisioning functions

---
+++ provision.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/env groovy
+
+def cfTemplateValidate(cfTemplatesRepoUrl='1', cfBranch='2', cfTemplateFile='3') {
+
+    docker.image('mesosphere/aws-cli').inside('-v /var/run/docker.sock:/var/run/docker.sock') {
+
+        git branch: "${cfBranch}",  url: "${cfTemplatesRepoUrl}"
+
+        stage('CF Temlate validate') {
+
+			// sh -e to prevent printing variables in log
+			sh "#!/bin/sh -e\n" + "export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} && export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}"
+            sh "aws cloudformation --region eu-west-1 validate-template --template-body file://${cfTemplateFile}"
+        }
+    }
+}
+
+return this
