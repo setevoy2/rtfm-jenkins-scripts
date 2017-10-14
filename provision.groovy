862914cf9322a953ddbf4a6793cb9eadc5728ecb
Arseny Zinchenko <reg@setevoy.kiev.ua>
2017-10-14 18:47:34 +0300

    provisioning functions

--- provision.groovy
+++ provision.groovy
@@ -8,7 +8,8 @@ def cfTemplateValidate(cfTemplatesRepoUrl='1', cfBranch='2', cfTemplateFile='3')
 
         stage('CF Temlate validate') {
 
-			// sh -e to prevent printing variables in log
+			// sh -e to prevent printing variables in a build log
+			// read https://stackoverflow.com/a/39908900/2720802
 			sh "#!/bin/sh -e\n" + "export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} && export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}"
             sh "aws cloudformation --region eu-west-1 validate-template --template-body file://${cfTemplateFile}"
         }
